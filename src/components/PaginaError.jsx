cac4cfe0c27b9d1f59f3b4428e26a3dce60512c3
MercedesQuinterno <mercedes.quinterno89@gmail.com>
2023-05-29 18:14:00 -0300

    error 404

---
+++ src/components/PaginaError.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Box from "@mui/material/Box";
+const PaginaError = () => {
+  return (
+    <Box
+
+    >
+      <img
+        src="https://media.istockphoto.com/id/492650846/vector/error-page.jpg?s=612x612&w=0&k=20&c=9AD616TGxJFXvhCvhMNILVNFh4z5dL2k9IHoNLO_3y0="
+        alt="Error"
+      />
+    </Box>
+  );
+};
+
+export default PaginaError;
